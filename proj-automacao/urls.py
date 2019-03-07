480e9381e5ebc7793fe425e538e8688735b83555
lclpsoz <lclpsoz@gmail.com>
2019-03-07 14:39:38 -0300

    Update interface to new version made by Ivan.

--- proj-automacao/urls.py
+++ proj-automacao/urls.py
@@ -13,5 +13,14 @@ urlpatterns = [
     path("cozinha/", proj.views.upd, name="cozinha"),
     path("quarto/", proj.views.upd, name="quarto"),
     path("wc/", proj.views.upd, name="wc"),
-    path("admin/", admin.site.urls),
+    
+    path("lamp-quarto/", proj.views.upd, name="lamp-quarto"),
+    path("lamp-sala/", proj.views.upd, name="lamp-sala"),
+    path("lamp-cozinha/", proj.views.upd, name="lamp-cozinha"),
+    path("lamp-garagem/", proj.views.upd, name="lamp-garagem"),
+    path("lamp-ext/", proj.views.upd, name="lamp-ext"),
+    path("garagem/", proj.views.upd, name="garagem"),
+    path("alarme/", proj.views.upd, name="alarme"),
+    
+    path("admin/", admin.site.urls)
 ]
--- proj/views.py
+++ proj/views.py
@@ -1,56 +1,50 @@
 import requests
 from django.http import HttpResponse
 
+
 def index(request):
 	with open ("sistema.html", "r") as f:
 		s = f.read()
 
 	return HttpResponse(s)
 
-def fInv (s, pos):
-	if (s[pos] == 1):
-		s[pos] = 0
-	else:
-		s[pos] = 1
-		
-	return s
-
 def upd(request):
-	s = str (request).split ('/')[1]
+	lampOn = 'https://www.w3schools.com/js/pic_bulbon.gif'
+	lampOff = 'https://www.w3schools.com/js/pic_bulboff.gif'
+	garagemOn = 'https://visualpharm.com/assets/4/Garage%20Open-595b40b65ba036ed117d2d57.svg'
+	garagemOff = 'https://visualpharm.com/assets/971/Garage%20Closed-595b40b65ba036ed117d2d56.svg'
+	alarmeOn = 'https://image.flaticon.com/icons/png/128/148/148921.png'
+	alarmeOff = 'https://image.flaticon.com/icons/png/128/149/149845.png'
+	idTag = str (request).split ('/')[1]
+
+	src = ["", ""]
+	if (idTag[0:4] == "lamp"):
+		src[0] = lampOff
+		src[1] = lampOn
+	elif (idTag == "garagem"):
+		src[0] = garagemOff
+		src[1] = garagemOn
+	elif (idTag == "alarme"):
+		src[0] = alarmeOn
+		src[1] = alarmeOff
 	
-	with open ("info.txt", "r") as f:
-		l = list (f.read())
-		info = [int (x) for x in l]
-	
-	if (s == 'sala'):
-		info = fInv (info, 0)
-	elif (s == 'cozinha'):
-		info = fInv (info, 1)
-	elif (s == 'quarto'):
-		info = fInv (info, 2)
-	else:
-		info = fInv (info, 3)
-		
-	with open ("info.txt", "w") as f:
-		out = ""
-		for i in info:
-			out += str (i)
-		f.write (out)
-	
-	pattern = "/*" + s + "*/"
 	with open ("sistema.html", "r") as f:
-		fi = f.read()
-		fi = fi.split (pattern)
-		if (fi[1][0] == 'b'): # black
-			fi[1] = fi[1][5:]
-			inv = "yellow"
-		else: # yellow
-			fi[1] = fi[1][6:]
-			inv = "black"
+		fi = str (f.read())
+		patOff = idTag + '" src="' + src[0]
+		patOn = idTag + '" src="' + src[1]
+		print (patOff + " VS " + patOn)
 		
+		if (patOff in fi):
+			fi = fi.split (patOff)
+			pattern = patOn
+		else:
+			fi = fi.split (patOn)
+			pattern = patOff
+		
+	print ("NEW: " + str (pattern))
 	with open ("sistema.html", "w") as f:
-		f.write (fi[0] + pattern + inv + fi[1])
-		
+		f.write (fi[0] + pattern + fi[1])
+
 	return index (request)
 
 def info(request):
